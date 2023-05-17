cffdcdf389c95323b9aaea6d50132d94fda4a26a
veljkoza <veljko.sekulic1998@gmail.com>
2023-05-17 23:08:19 +0200

    Makes route for fetching industries private

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/for-companies", "/pricing"],
+  publicRoutes: [
+    "/",
+    "/about",
+    "/for-companies",
+    "/pricing",
+    "/interview-creator",
+    "/api/trpc/industries.getAll",
+  ],
 });
 
 export const config = {
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,10 +1,6 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -17,9 +13,5 @@ export const exampleRouter = createTRPCRouter({
 
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
   }),
 });
