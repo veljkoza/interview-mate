e4bf217cf4605b04ccfa0ff5b233333348ad3ee2
veljkoza <veljko.sekulic1998@gmail.com>
2023-10-02 20:11:37 +0200

    next public variable

--- src/domain/Config.ts
+++ src/domain/Config.ts
@@ -3,6 +3,6 @@ import { env } from "~/env.mjs";
 export const Config = {
   mockInterview: {
     maximumSpeechRecognitionDuration:
-      +env.MAXIMUM_SPEECH_RECOGNITION_DURATION || 70,
+      +env.NEXT_PUBLIC_MAXIMUM_SPEECH_RECOGNITION_DURATION || 70,
   },
 };
